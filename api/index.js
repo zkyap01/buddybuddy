4761a754ca259bb4d245117ce15c149bc2dbfe08
Yap Zhi Kuang <46211670+zkyap01@users.noreply.github.com>
2024-03-23 08:56:18 +0800

    s

---
+++ api/index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const router1 = require('./mintclub/mintclub');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/mintclub', router1);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ api/mintclub/mintclub.js
@@ -0,0 +1,49 @@
+const { mintclub } = require('mint.club-v2-sdk');
+
+const MortyMee6Nft = mintclub.network('base').nft('MnM-NFT');
+
+async function deployNFT() {
+  try {
+    // 🚀 Deploying $MNM-NFT tokens
+    await MortyMee6Nft.create({
+      name: 'Morty and Mee6',
+      // Base Network WETH
+      reserveToken: {
+        address: '0x4200000000000000000000000000000000000006',
+        decimals: 18,
+      },
+      // Bonding curve data
+      curveData: {
+        curveType: 'EXPONENTIAL',
+        stepCount: 10, // how granular the curve is
+        maxSupply: 10_000, // NFT max supply
+        initialMintingPrice: 0.01, // starting price, 0.01 WETH
+        finalMintingPrice: 0.1, // ending price, 0.1 WETH
+        creatorAllocation: 100, // initial supply to the deployer
+      },
+      metadataUrl: 'ipfs://...'
+    });
+
+    console.log('NFT deployed successfully.');
+  } catch (error) {
+    console.error('Error deploying NFT:', error);
+  }
+}
+
+const express = require('express');
+const router = express.Router();
+
+// Middleware function
+const exampleMiddleware = (req, res, next) => {
+  console.log('Example middleware');
+  next();
+};
+
+// Route to get all users
+router.get('/', exampleMiddleware, (req, res) => {
+    // Call the function to deploy the NFT
+    deployNFT();
+    res.json("Test End");
+});
+
+module.exports = router;
