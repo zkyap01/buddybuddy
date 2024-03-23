521f43b1b58f34128a90baf102abb243fe5d6692
Yap Zhi Kuang <46211670+zkyap01@users.noreply.github.com>
2024-03-23 16:08:51 +0800

    s

--- api/mintclub/mintclub.js
+++ api/mintclub/mintclub.js
@@ -1,4 +1,10 @@
-const { mintclub } = require('mint.club-v2-sdk');
+const {
+  BOND_ABI,
+  chainStringToId,
+  generateCreateArgs,
+  getMintClubContractAddress,
+  mintclub,
+} = require('mint.club-v2-sdk');
 const { createWalletClient, custom, http, publicActions } = require('viem');
 const { mainnet, sepolia } = require('viem/chains')
 const { privateKeyToAccount }  = require('viem/accounts')
@@ -8,12 +14,32 @@ const address = '0x1671aad14B578C74259b682fac2111845BD0964D'
 const chain = 11155111
 const tmpname = "zkkks"
 
+// const { tokenParams, bondParams } = generateCreateArgs({
+//   name: tmpname,
+//   reserveToken: {
+//     address: '0xb16f35c0ae2912430dac15764477e179d9b9ebea',
+//     decimals: 18,
+//   },
+//   tmpname:tmpname,
+//   tokenType: 'ERC20',
+//   curveData: {
+//     curveType: 'EXPONENTIAL',
+//     stepCount: 10, // how granular the curve is
+//     maxSupply: 10_000, // NFT max supply
+//     initialMintingPrice: 0.01, // starting price, 0.01 WETH
+//     finalMintingPrice: 0.1, // ending price, 0.1 WETH
+//     creatorAllocation: 100, // initial supply to the deployer
+//   },
+// });
+
+
 const account = privateKeyToAccount(pk)
 const client = createWalletClient({
   account,
   chain: sepolia,
   transport: http('https://rpc.sepolia.org')
 }).extend(publicActions)
+
 
 const MortyMee6Nft = mintclub
   // .network(chain)
@@ -41,7 +67,17 @@ const MortyMee6Nft = mintclub
 
 async function deployNFT(addresses, name) {
   try {
+    // const creationFee = await mintclub.network('sepolia').bond.getCreationFee();
 
+    // client.writeContract({
+    //     abi: BOND_ABI,
+    //     chainId: sepolia.id,
+    //     functionName: 'createToken',
+    //     to: getMintClubContractAddress('BOND', sepolia.id),
+    //     args: [tokenParams, bondParams],
+    //     value: creationFee,
+    // });
+  
 
     // 🚀 Deploying $MNM-NFT tokens
     await MortyMee6Nft.create({
