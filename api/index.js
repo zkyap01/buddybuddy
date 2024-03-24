144c6803246905a2c093249a1e1ff48602ec4a35
Kenny Yong <homeyong@gmail.com>
2024-03-24 10:48:16 +0800

    test

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const router1 = require('./mintclub/mintclub'); //http://localhost:3000/mintclub/
+const router1 = require('./circle/circle'); //http://localhost:3000/mintclub/
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/mintclub', router1);
+app.use('/circle', router1);
 
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
